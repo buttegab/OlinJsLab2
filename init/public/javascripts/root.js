a3a205d325b4aa387ae12e4a853ef01f792a61fa
buttegab <gabriel.butterick@students.olin.edu>
2016-03-04 16:29:43 -0500

    reformatted and removed things, laid various front-end frameworks

---
+++ init/public/javascripts/root.js
@@ -0,0 +1,18 @@
+var app = angular.module("bookQuestApp", ['ngRoute']);
+
+//Handles all the angular controllers and data acquiring from the routes.
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+    $scope.currentSkwiki = null;
+
+    // when landing on the page, get all skwiki titles and show them
+    $http.get("/")
+        .success(function(data) {
+            $scope.books = data;
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+}
